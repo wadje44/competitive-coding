d9357ea8c2aa913bd908963d9799f664b0a47ad7
wadje44 <shashankwadje44@gmail.com>
2025-12-02 11:42:27 +0100

    fix: output format aoc-2025-day-1

--- challenges/aoc-2025/day-1/problem1/Solution.java
+++ challenges/aoc-2025/day-1/problem1/Solution.java
@@ -37,9 +37,9 @@ class Solution {
             counter++;
           }
         }
-        System.out.println("input " + (fi + 1) + " answer is: " + counter);
+        System.out.println("input " + listOfFiles[fi].getName() + " answer is: " + counter);
         BufferedWriter writer = new BufferedWriter(
-            new FileWriter("./test-cases/output/" + (fi + 1) + ".txt"));
+            new FileWriter("./test-cases/output/" + listOfFiles[fi].getName()));
         writer.write(" answer is: " + counter);
         writer.close();
       }
